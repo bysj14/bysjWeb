1b97598dda72c81d2989e9dcbaab5802ae2aaea5
ZXzxxx <532721245@qq.com>
2018-01-29 12:27:08 +0800

    first commit

---
+++ src/main/java/com/xg/techjoy/BysjWebApplication.java
@@ -0,0 +1,12 @@
+package com.xg.techjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BysjWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BysjWebApplication.class, args);
+	}
+}
